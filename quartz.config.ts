e7efbc2e5e9b43544c60b654f481dd776d97cc08
unknown <justintimber949@gmail.com>
2025-09-29 13:59:31 +0700

    Quartz sync: Sep 29, 2025, 1:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "note-preclinic.publichealth",
+    baseUrl: "main",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
